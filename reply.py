b6b26b1295b2dfab0bbcf24cd45986d6a37184d7
Ester Ramos <esterramos48@gmail.com>
2017-11-25 14:55:37 +0000

    Add a reply feature

---
+++ reply.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from twbot import api
+import tweepy
+import time
+
+def reply():
+    for tweet in tweepy.Cursor(api.mentions_timeline).items():
+        try:
+            #print('tweet from @' + tweet.user.screen_name + ':' + tweet.text)
+            api.update_status('@' + tweet.user.screen_name + ' cheers, love!', in_reply_to_status_id = tweet.id)
+
+        except tweepy.TweepError as e:
+            print(e.reason)
+
+        except StopIteration:
+            break
+
+
+while True:
+    try:
+
+        reply()
+        time.sleep(60)
+
+    except KeyboardInterrupt as e:
+        break
+    #rate limiting exception, we back off
+    except tweepy.TweepError as e:
+        if e.api_code == 429:
+            time.sleep(300)
+        else:
+            raise
